49ce80ad5a790d14597567224c91a180f645a5d1
Miad Aleali <miad.aleali@nera-ai.de>
2024-10-06 14:14:39 +0200

    test

--- api/index.php
+++ api/index.php
@@ -1,24 +1,11 @@
 <?php
 // index.php
-
-// فعال‌سازی نمایش خطاها برای دیباگینگ
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// بارگذاری اتولودر کامپوزر
 require_once 'vendor/autoload.php';
-
-// بارگذاری متغیرهای محیطی با استفاده از Dotenv
 use Dotenv\Dotenv;
 use App\Handlers\ExceptionHandler;
-
-// بارگذاری متغیرهای محیطی
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-// بررسی وجود RECAPTCHA_SECRET_KEY
-if (!isset($_ENV['RECAPTCHA_SECRET_KEY']) || empty($_ENV['RECAPTCHA_SECRET_KEY'])) {
+if (!isset($_ENV['6LdypVMqAAAAALEMKytEtDHnHPxGKcLkaz_-R_CU']) || empty($_ENV['RECAPTCHA_SECRET_KEY'])) {
     throw new \Exception('RECAPTCHA_SECRET_KEY is not set in the environment variables.');
 }
 
